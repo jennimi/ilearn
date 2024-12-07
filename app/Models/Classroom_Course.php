0a24e995f00bbce2095d2e71d693298166a89b13
jasonmiracleg <jasonmiracle15@gmail.com>
2024-12-08 00:01:35 +0700

    Create Tables, Models, and Controller

---
+++ app/Models/Classroom_Course.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom_Course extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClassroomCourseFactory> */
+    use HasFactory;
+}
